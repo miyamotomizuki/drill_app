11b252e7775c6f8ce8320c7644851f662dca4511
miyamotomizuki <miiiisun@icloud.com>
2023-05-05 15:08:23 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+  
+  def index
+    @post = Post.all
+  end
+
+  def new
+    
+  end
+
+  def create
+    Post.create(memo: params[:memo])
+  end
+end
